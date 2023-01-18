14d65f992cba7b81be369cb1737460c8c9f8becc
wilmarques <wileymarques@live.com>
2023-01-18 12:35:04 +0000

    WIP - feat: Add platforms
    - https://codelabs.developers.google.com/codelabs/flutter-flame-game#4

--- lib/game/managers/object_manager.dart
+++ lib/game/managers/object_manager.dart
@@ -25,9 +25,53 @@ class ObjectManager extends Component with HasGameRef<DoodleDash> {
   final double _tallestPlatformHeight = 50;
   final List<Platform> _platforms = [];
 
-  // Add Platforms: Add onMount method
+  @override
+  void onMount() {
+    super.onMount();
 
-  // Add Platforms: Add update method
+    var currentX = (gameRef.size.x.floor() / 2).toDouble() - 50;
+    var currentY =
+        gameRef.size.y - (_rand.nextInt(gameRef.size.y.floor()) - 3) - 50;
+
+    for (var i = 0; i < 9; i++) {
+      if (i != 0) {
+        currentX = _generateNextX(100);
+        currentY = _generateNextY();
+      }
+      _platforms.add(
+        _semiRandomPlatform(
+          Vector2(currentX, currentY),
+        ),
+      );
+
+      add(_platforms[i]);
+    }
+  }
+
+  @override
+  void update(double dt) {
+    final topOfLowestPlatform =
+        _platforms.first.position.y + _tallestPlatformHeight;
+
+    final screenBottom = gameRef.player.position.y +
+        (gameRef.size.x / 2) +
+        gameRef.screenBufferSpace;
+
+    if (topOfLowestPlatform > screenBottom) {
+      var newPlatY = _generateNextY();
+      var newPlatX = _generateNextX(100);
+      final nextPlat = _semiRandomPlatform(Vector2(newPlatX, newPlatY));
+      add(nextPlat);
+
+      _platforms.add(nextPlat);
+
+      gameRef.gameManager.increaseScore();
+
+      _cleanupPlatforms();
+    }
+
+    super.update(dt);
+  }
 
   final Map<String, bool> specialPlatforms = {
     'spring': true, // level 1
@@ -99,7 +143,9 @@ class ObjectManager extends Component with HasGameRef<DoodleDash> {
     return currentHighestPlatformY - distanceToNextY;
   }
 
-  // Add platforms: Add _semiRandomPlatform method
+  Platform _semiRandomPlatform(Vector2 position) {
+    return NormalPlatform(position: position);
+  }
 
   // Losing the game: Add enemy code
 
