2a520d54f4e858c2b5032663499e8cd574342454
wilmarques <wileymarques@live.com>
2023-01-30 11:45:55 +0000

    feat: Mobile support
    - https://codelabs.developers.google.com/codelabs/flutter-flame-game#10

--- lib/game/widgets/game_overlay.dart
+++ lib/game/widgets/game_overlay.dart
@@ -2,9 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:io' show Platform;
 import 'package:flame/game.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 
 import '../doodle_dash.dart';
@@ -21,7 +19,6 @@ class GameOverlay extends StatefulWidget {
 
 class GameOverlayState extends State<GameOverlay> {
   bool isPaused = false;
-  // Mobile Support: Add isMobile boolean
 
   @override
   Widget build(BuildContext context) {
@@ -57,7 +54,51 @@ class GameOverlayState extends State<GameOverlay> {
               },
             ),
           ),
-          // Mobile Support: Add on-screen left & right directional buttons
+          Positioned(
+            bottom: MediaQuery.of(context).size.height / 4,
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 24),
+                    child: GestureDetector(
+                      onTapDown: (details) {
+                        (widget.game as DoodleDash).player.moveLeft();
+                      },
+                      onTapUp: (details) {
+                        (widget.game as DoodleDash).player.resetDirection();
+                      },
+                      child: Material(
+                        color: Colors.transparent,
+                        elevation: 3.0,
+                        shadowColor: Theme.of(context).colorScheme.background,
+                        child: const Icon(Icons.arrow_left, size: 64),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 24),
+                    child: GestureDetector(
+                      onTapDown: (details) {
+                        (widget.game as DoodleDash).player.moveRight();
+                      },
+                      onTapUp: (details) {
+                        (widget.game as DoodleDash).player.resetDirection();
+                      },
+                      child: Material(
+                        color: Colors.transparent,
+                        elevation: 3.0,
+                        shadowColor: Theme.of(context).colorScheme.background,
+                        child: const Icon(Icons.arrow_right, size: 64),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
           if (isPaused)
             Positioned(
               top: MediaQuery.of(context).size.height / 2 - 72.0,
